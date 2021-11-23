3a19f619873d0931b5613432ebc4361a72ab4bc2
Rohit Handique <rohit.handique.rh@gmail.com>
2021-11-23 23:28:41 +0530

    new changes

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -4,15 +4,15 @@ import abi from "../utils/abi.json";
 import {
   FormControl, FormLabel, Input, Button, Container
 } from "@chakra-ui/react"
+import { ipfsCid } from "../helpers/ipfsCid";
 
 function Upload() {
 
-    console.log(process.env.REACT_APP_API_KEY)
-
-    const contract_address = "0x4306dce14cEd3B5Aac553C29Fa465322342707A4";
+    const contract_address = "0x73d4Ebde470A7C8ec36612a23594a5Aa1f27660d";
     const fileInputRef = useRef();
     const nameRef = useRef();
     const descriptionRef = useRef();
+    let cid = ""
 
     const mintNft = async (_name, _description, _url) => {
         try {
@@ -41,35 +41,62 @@ function Upload() {
         } catch (error) {
           console.log(error)
         }
-    }  
+      }
 
-    const onUpload = (e) => {
+    const onUpload = async (e) => {
         e.preventDefault();
-        console.log(fileInputRef.current.files[0])
         const formData = new FormData();
         formData.append(  
         "file",
         fileInputRef.current.files[0],
         );
 
+        const hash = await ipfsCid(fileInputRef.current.files[0]);
+        console.log(hash._baseCache.get('b'));
+        const NFTname = nameRef.current.value;
+        const NFTdescription = descriptionRef.current.value;
+        const NFTurl = hash._baseCache.get('b');
+
         fetch("https://api.nft.storage/upload", {
         method: "POST",
         body: formData,
         headers: {
-            'Authorization': 'Bearer '+process.env.REACT_APP_API_KEY
+            'Authorization': 'Bearer '+process.env.REACT_APP_TEST_API_KEY
         }
         })
         .then(response => response.json())
-        .then(json => {
+        .then(() => {
+
+          const data = {
+            "name": nameRef.current.value,
+            "description": descriptionRef.current.value,
+            "image": "ipfs://"+hash._baseCache.get('b'),
+          } ;
+          
+          let formData  = new FormData();
+
+          formData.append("name",nameRef.current.value);
+          formData.append("description",descriptionRef.current.value);
+          formData.append("image", "ipfs://"+hash._baseCache.get('b'));
+
+          console.log(formData)
+
+          /*fetch("https://api.nft.storage/store",{
+            method: "POST",
+            body: formData, 
+            headers: {
+              'Authorization': 'Bearer '+process.env.REACT_APP_TEST_API_KEY,
+            }
+          }).then(response => response.json())
+          .then((json)=>{
             console.log(json)
-            const NFTname = nameRef.current.value;
-            const NFTdescription = descriptionRef.current.value;
-            const NFTurl = json.value.cid + "/" + fileInputRef.current.files[0].name;
-            console.log(NFTname)
-            console.log(NFTdescription)
-            mintNft(NFTname, NFTdescription, NFTurl);
+            
+            //mintNft();
+          })*/
         });
     }
+
+    
 
     return (
         <Container mt={40}>
